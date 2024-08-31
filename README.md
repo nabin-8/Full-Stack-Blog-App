90993edbdb54fd48b8445809d6c34866666b19d2
nabin-8 <nabinacharya740@gmail.com>
2024-08-31 08:16:14 +0545

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Full-Stack-Blog-App
 Full Stack Blog App Using MERN Stack.
+
+### Installation and Setup
+1. npx create-react-app
+1. 
---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1 className=" text-center bg-slate-800">Hello react</h1>
+    </>
+  );
+}
+
+export default App;
